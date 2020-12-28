70b25b5ef4ba18c27fa924288dfb62f814e36163
CzompiBOT <czompi@czompi.hu>
2020-12-28 20:53:45 +0100

    Updated to the new API & changed the UI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using hu.hunluxlauncher.libraries.auth.microsoft;
 
-namespace Libraries.Auth.Sample
+namespace Libraries.Auth.Microsoft.Sample
 {
     /// <summary>
     /// Interaction logic for App.xaml
@@ -16,6 +11,16 @@ namespace Libraries.Auth.Sample
     {
         public App()
         {
+            MSAuth = new MicrosoftAuthentication(AuthSettings);
         }
+
+        public static MicrosoftAuthentication MSAuth { get; internal set; }
+        public static AuthenticationSettings AuthSettings { get; } = new AuthenticationSettings()
+        {
+            UserAgent = "HunLux Launcher Authentication Test Environment/20w53a",
+            ClientId = "00000000-0000-0000-0000-000000000000",
+            BrowserRedirectSuccess = new Uri("https://auth.hunluxlauncher.hu/microsoft/success"),
+            BrowserRedirectError = new Uri("https://auth.hunluxlauncher.hu/microsoft/error")
+        };
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,96 +1,63 @@
 ﻿using hu.hunluxlauncher.libraries.auth.microsoft;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Text.Json;
-using System.Threading;
 using System.Windows;
 
-namespace Libraries.Auth.Sample
+namespace Libraries.Auth.Microsoft.Sample
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public AuthenticationSettings AuthSettings { get; }
 
-        readonly hu.hunluxlauncher.libraries.auth.microsoft.Auth AuthInfo;
         public MainWindow()
         {
             InitializeComponent();
-            AuthSettings = new AuthenticationSettings
-            {
-                UserAgent = Title.Replace(" - ", "/")
-            };
-            AuthInfo = new hu.hunluxlauncher.libraries.auth.microsoft.Auth(AuthSettings);
-            Uri uri;
-            bool alreadyLoggedIn = false;
-            if (!alreadyLoggedIn) uri = AuthInfo.LoginInit();
-            else uri = AuthInfo.LogoutInit();
-
-            viewer.Navigate(uri);
-            viewer.Navigating += Viewer_Navigating;
-            BackgroundWorker backgroundWorker = new(); Thread thread = null;
-            List<string> errors = new();
-            backgroundWorker.DoWork += (sender, e) =>
-            {
-                thread = new Thread(() =>
-                {
-                    while (AuthorizationToken == null) ; // waiting for authorization to finish
-                    this.Dispatcher.Invoke(() => {
-                        viewer.Visibility = Visibility.Collapsed;
-                        preview.Visibility = Visibility.Visible;
-                    });
-                    XboxLiveAuthenticate = AuthInfo.XboxLiveAuthenticate(AuthorizationToken.AccessToken);
-                    XSTSAuthenticate = AuthInfo.XSTSAuthenticate(XboxLiveAuthenticate.Token);
-                    MinecraftAuthenticate = AuthInfo.MinecraftAuthenticate(XboxLiveAuthenticate.DisplayClaims.XuiClaims[0].UserHash, XSTSAuthenticate.Token);
-                    GameOwnership = AuthInfo.CheckingGameOwnership(MinecraftAuthenticate.AccessToken);
-                    try
-                    {
-                        ProfileInfo = AuthInfo.GetProfile(MinecraftAuthenticate.AccessToken);
-                    }
-                    catch (UnauthorizedAccessException)
-                    {
-                        errors.Add("The Microsoft account, that you're trying to login with, does not own the game.");
-                    }
-
-                    this.Dispatcher.Invoke(() => {
-                        // authentication process finished, do something after it...
-                        if(errors.Count == 0)
-                        {
-                            if(ProfileInfo != null)
-                            {
-                                preview.Text = $"Profile info:\r\n{JsonSerializer.Serialize(ProfileInfo)}";
-                            }
-                            else
-                            {
-                                errors.Add("The Microsoft account, that you're trying to login with, does not own the game.");
-                            }
-                        }
-                        preview.Text = string.Join("\r\n", errors);
-                        viewer.Navigate(AuthInfo.LogoutInit());
-                    });
-                });
-                if (!thread.IsAlive) thread.Start();
-            };
-            backgroundWorker.RunWorkerAsync();
+            Title = App.AuthSettings.UserAgent.Replace("/", " - ");
         }
 
-        public hu.hunluxlauncher.libraries.auth.microsoft.account.TokenResponse AuthorizationToken { get; private set; } = null;
-        public hu.hunluxlauncher.libraries.auth.microsoft.xbox.TokenResponse XboxLiveAuthenticate { get; private set; } = null;
-        public hu.hunluxlauncher.libraries.auth.microsoft.xbox.TokenResponse XSTSAuthenticate { get; private set; } = null;
-        public hu.hunluxlauncher.libraries.auth.microsoft.minecraft.Authenticate MinecraftAuthenticate { get; private set; } = null;
-        public hu.hunluxlauncher.libraries.auth.microsoft.minecraft.GameOwnership GameOwnership { get; private set; } = null;
-        public hu.hunluxlauncher.libraries.auth.microsoft.minecraft.ProfileInfo ProfileInfo { get; private set; } = null;
-
-        private void Viewer_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        /// <summary>
+        /// Call AcquireToken - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
         {
-            var data = AuthInfo.CatchAuthorizationToken(e.Uri);
-            if (data != null)
+            try
             {
-                AuthorizationToken = data;
+                ResultText.Text = "Authentication process started...";
+                TokenInfoText.Text = "";
+                var msacc = await App.MSAuth.SignIn();
+
+                var xbaut = new XboxAuthentication(App.AuthSettings, msacc);
+                var mcaut = new MinecraftAuthentication(App.AuthSettings, xbaut);
+                ResultText.Text = "Authentication finished!";
+                TokenInfoText.Text += $"XBL Token:\r\n{xbaut.XboxLive.Token}\r\n\r\n";
+                TokenInfoText.Text += $"XSTS token:\r\n{xbaut.XboxSecurityTokenService.Token}\r\n\r\n";
+                TokenInfoText.Text += $"Minecraft:\r\n{JsonSerializer.Serialize(mcaut.Minecraft, new JsonSerializerOptions { WriteIndented = true })}\r\n\r\n";
+                CallGraphButton.IsEnabled = false;
+                SignOutButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Authentication falied!";
+                TokenInfoText.Text = $"{ex}";
+            }
+        }
+        /// <summary>
+        /// Sign out the current user
+        /// </summary>
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(await App.MSAuth.SignOut())
+                {
+                    ResultText.Text = "User has signed-out";
+                    TokenInfoText.Text = $"";
+                    CallGraphButton.IsEnabled = true;
+                    SignOutButton.IsEnabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                TokenInfoText.Text = $"{ex}";
             }
         }
     }
